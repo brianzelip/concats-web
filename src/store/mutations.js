06195125f3bc8844ef8f2c2496bf12990daa0cd1
Brian Zelip <bzelip@gmail.com>
2018-12-08 17:31:36 -0500

    scaffold basic mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,1 +1,17 @@
-//mutations
+import Vue from 'vue';
+
+export const SET_CSV = (state, payload) => {
+  state.csv = payload;
+};
+
+export const SET_CSV_HEADERS = (state, payload) => {
+  state.csvHeaders = payload;
+};
+
+export const RESET_CSV = state => {
+  state.csv = '';
+};
+
+export const RESET_CSV_HEADERS = state => {
+  state.csvHeaders = [];
+};
