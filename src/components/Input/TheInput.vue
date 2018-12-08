4194f70f504324c7c176098943340bbcb7dea758
Brian Zelip <bzelip@gmail.com>
2018-12-08 08:18:13 -0500

    add label to file input

--- src/components/Input/TheInput.vue
+++ src/components/Input/TheInput.vue
@@ -1,7 +1,10 @@
 <template>
-  <input
-    id
-    name
-    type="file"
-  >
+  <section>
+    <label for="fileInput">Select csv file:</label>
+    <input
+      id="fileInput"
+      name="fileInput"
+      type="file"
+    >
+  </section>
 </template>
