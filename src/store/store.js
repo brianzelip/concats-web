6699abb64446d849742b4497bbf3f7b2a2869a9f
Brian Zelip <bzelip@gmail.com>
2018-12-08 08:17:30 -0500

    add store actions and mutations

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,16 @@ import Vuex from 'vuex';
 
 import * as state from './state.js';
 import * as getters from './getters.js';
+import * as actions from './actions.js';
+import * as mutations from './mutations.js';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state,
-  getters
+  getters,
+  actions,
+  mutations
 });
 
 export default store;
