9329dbf261646bbb32b7c92b75114f9eb85b4d72
Brian Zelip <bzelip@gmail.com>
2018-12-08 14:44:22 -0500

    clean up store files to take vuex temp out of the equation

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,15 +1,16 @@
 export const concat = state => {
-  return state.data
-    .reduce((acc, obj) => {
-      const keys = ['Group Name', 'Meeting Rep Name', 'Meeting Rep Phone'];
-      var concattedString = '';
-      keys.forEach((key, index) => {
-        index !== 2
-          ? (concattedString = concattedString.concat(`${obj[key]} `))
-          : (concattedString = concattedString.concat(obj[key]));
-      });
-      acc.push(concattedString);
-      return acc;
-    }, [])
-    .join('\n');
+  // return state.data
+  //   .reduce((acc, obj) => {
+  //     const keys = ['Group Name', 'Meeting Rep Name', 'Meeting Rep Phone'];
+  //     var concattedString = '';
+  //     keys.forEach((key, index) => {
+  //       index !== 2
+  //         ? (concattedString = concattedString.concat(`${obj[key]} `))
+  //         : (concattedString = concattedString.concat(obj[key]));
+  //     });
+  //     acc.push(concattedString);
+  //     return acc;
+  //   }, [])
+  //   .join('\n');
+  return state.data;
 };
--- src/store/store.js
+++ src/store/store.js
@@ -10,9 +10,7 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state,
-  getters,
-  actions,
-  mutations
+  getters
 });
 
 export default store;
